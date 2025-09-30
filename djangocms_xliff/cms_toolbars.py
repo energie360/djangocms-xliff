2194caa880ef4e4213b587256b4869a76925b38d
André da Silva <andre-da.silva@outlook.com>
2025-09-30 16:01:27 +0200

    Update cms toolbar

--- djangocms_xliff/cms_toolbars.py
+++ djangocms_xliff/cms_toolbars.py
@@ -1,21 +1,23 @@
 from functools import partial
 
 from cms.cms_toolbars import LANGUAGE_MENU_IDENTIFIER
-from cms.models import Page
+from cms.models import PageContent
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
-from cms.utils import page_permissions
+from cms.utils.page_permissions import user_can_change_page
+from cms.utils.permissions import get_model_permission_codename
 from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
 from django.utils.translation import gettext
+from djangocms_alias.models import AliasContent
 
 XLIFF_NAMESPACE = "djangocms_xliff"
 XLIFF_LANGUAGE_BREAK = "Export XLIFF Break"
 
 
 class XliffToolbar(CMSToolbar):
-    def populate(self):
-        super().populate()
+    def post_template_populate(self):
+        super().post_template_populate()
         obj = self.get_object()
         if obj and self.user_has_permissions(obj=obj):
             self.update_language_menu(obj=obj)
@@ -51,16 +53,35 @@ class XliffToolbar(CMSToolbar):
             reverse_xliff(viewname="djangocms_xliff:upload"),
         )
 
+    def is_page_content(self, obj) -> bool:
+        return type(obj) is PageContent
+
+    def is_alias_content(self, obj) -> bool:
+        return type(obj) is AliasContent
+
 
 @toolbar_pool.register
-class XliffPageToolbar(XliffToolbar):
+class XliffPageContentToolbar(XliffToolbar):
     def user_has_permissions(self, obj) -> bool:
-        if hasattr(obj, "_wrapped"):  # request.current_page returns a SimpleLazyObject
-            obj = obj._wrapped
+        if not self.toolbar.edit_mode_active or not obj or not self.is_page_content(obj):
+            return False
 
-        if self.toolbar.edit_mode_active and obj and type(obj) is Page and len(obj.get_languages()) > 1:
-            return page_permissions.user_can_change_page(user=self.request.user, page=obj, site=self.current_site)
-        return False
+        if len(obj.page.get_languages()) <= 1:
+            return False
+
+        return user_can_change_page(user=self.request.user, page=obj.page, site=self.current_site)
+
+
+@toolbar_pool.register
+class XliffAliasContentToolbar(XliffToolbar):
+    def user_has_permissions(self, obj) -> bool:
+        if not self.toolbar.edit_mode_active or not obj or not self.is_alias_content(obj):
+            return False
+
+        if len(obj.alias.get_languages()) <= 1:
+            return False
+
+        return self.request.user.has_perm(get_model_permission_codename(AliasContent, "change"))
 
 
 class XliffModelToolbar(XliffToolbar):
@@ -69,6 +90,7 @@ class XliffModelToolbar(XliffToolbar):
     """
 
     def user_has_permissions(self, obj) -> bool:
-        if self.toolbar.edit_mode_active and obj and type(obj) is not Page:
-            return self.request.user.has_perm(f"{obj._meta.app_label}.change_{obj._meta.model_name}")
-        return False
+        if not self.toolbar.edit_mode_active or not obj:
+            return False
+
+        return self.request.user.has_perm(f"{obj._meta.app_label}.change_{obj._meta.model_name}")
